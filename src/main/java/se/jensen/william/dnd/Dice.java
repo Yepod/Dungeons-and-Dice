14bf1338522cadc613f34be31ac88c7b9b28a93a
Yepod <Williamrossander@hotmail.com>
2025-09-29 20:36:28 +0200

    Simplyfied our Dice mechanics.

--- src/main/java/se/jensen/william/dnd/Dice.java
+++ src/main/java/se/jensen/william/dnd/Dice.java
@@ -3,22 +3,13 @@ package se.jensen.william.dnd;
 import java.util.Random;
 
 public class Dice {
-    private int diceRoll;
-
-    public Dice() {
-        roll();
-    }
 
     public int roll() {
         Random random = new Random();
-        this.diceRoll = random.nextInt(1, 7);
-        return diceRoll;
+        return random.nextInt(1, 7);
     }
 
-    public int getDiceRoll(){
-        return diceRoll;
-    }
-
+    // Total damage + Crit Function
     public int addToDmg(int roll1, int roll2){
         if (roll1 == 6 && roll2 == 6)
             return 24;
